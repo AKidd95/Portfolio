063fd0662a87fe0a4d83c5ac2050cce4dca1caec
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-07 12:58:23 +0100

    added spec files and formatting

--- spec/Feature/index_spec.rb
+++ spec/Feature/index_spec.rb
@@ -1,13 +1,10 @@
-require 'layout.rb'
-describe 'Index Page', type: :feature do
-  it 'displays project list' do
+require 'index.html.haml'
+
+it 'displays project list' do
     visit '/'
 
     expect(page).to have_css '.projects'
-
     within '.projects' do
       expect(page).to have_content 'My First Website'
-      expect(page).to have_content 'FizzBuzz'
     end
   end
-end
