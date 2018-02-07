aaa5e22ba609dd018f46c471e4285db8fe93b4ff
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-07 15:11:08 +0100

    added partials, bootstrap, and edited layout

--- spec/Feature/index_spec.rb
+++ spec/Feature/index_spec.rb
@@ -8,3 +8,13 @@ it 'displays project list' do
       expect(page).to have_content 'My First Website'
     end
   end
+
+  it 'renders footer partial' do
+    visit '/'
+
+    expect(page).to have_selector 'footer'
+    within 'footer' do
+      expect(page).to have_content 'My Portfolio'
+      expect(page).to have_content 'Built using the awesome Middleman framework'
+    end
+  end
