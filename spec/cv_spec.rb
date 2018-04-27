2b04ad7b1e8e683bf0ad6bb6444ffd6bfd3d29d5
AKidd95 <aiden.jubelin@gmail.com>
2018-04-27 19:23:04 +0200

    edit hard skills

---
+++ spec/cv_spec.rb
@@ -0,0 +1,32 @@
+describe 'CV page', type: :feature do
+  it 'displays contacts' do
+    visit '/cv.html'
+
+    expect(page).to have_css '.contact'
+    within '.contact' do
+      expect(page).to have_content 'Vivelvagen 25c, 125 33 Alvsjo'
+      expect(page).to have_content 'Aiden.jubelin@gmail.com'
+      expect(page).to have_content '+46 72-205 64 95'
+    end
+  end
+
+  it 'displays education' do
+    visit '/cv.html'
+
+    expect(page).to have_css '.education'
+    within '.education' do
+      expect(page).to have_content 'Craft Academy Bootcamp'
+      expect(page).to have_content '12 week full stack Web Development course.'
+    end
+  end
+
+  it 'displays hard_skills' do
+    visit '/cv.html'
+
+    expect(page).to have_css '.hard_skills'
+    within '.hard_skills' do
+      expect(page).to have_content 'Programming Languages'
+      expect(page).to have_content 'Ruby, Javascript, HTML, and CSS'
+    end
+  end
+end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -9,26 +9,3 @@ describe 'Index Page', type: :feature do
     end
   end
 end
-
-describe 'CV page', type: :feature do
-  it 'displays contacts' do
-    visit '/cv.html'
-
-    expect(page).to have_css '.contact'
-    within '.contact' do
-      expect(page).to have_content 'Vivelvagen 25c, 125 33 Alvsjo'
-      expect(page).to have_content 'Aiden.jubelin@gmail.com'
-      expect(page).to have_content '+46 72-205 64 95'
-    end
-  end
-
-  it 'displays education' do
-    visit '/cv.html'
-
-    expect(page).to have_css '.education'
-    within '.education' do
-      expect(page).to have_content 'Craft Academy Bootcamp'
-      expect(page).to have_content '12 week full stack Web Development course.'
-    end
-  end
-end
